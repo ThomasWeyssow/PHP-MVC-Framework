e780ca5f6462815333a6ab8c0407b3027041b9f2
Thomas Weyssow <thomas.weyssow@gmail.com>
2021-08-20 17:04:39 +0200

    Added views and layout

--- views/contact.php
+++ views/contact.php
@@ -11,7 +11,7 @@
     </div>
     <div class="mb-3">
         <label class="form-label">Body</label>
-        <textarea name="body" class="form-control"></textarea>
+        <textarea name="body" class="form-control"></textarea> 
     </div>
     <button type="submit" class="btn btn-primary">Submit</button>
 </form>
